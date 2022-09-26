5813a111caf32cc23c53d4461c52d89802d35899
Bobby Bates <robertfbatesjr@gmail.com>
2022-09-25 22:56:57 -0400

    update react routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import { useState, useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './Home'
 import NavBar from './NavBar'
+import SignUp from './SignUp'
+import SignIn from './SignIn'
+import MyTrips from './MyTrips'
+import AllTrips from './AllTrips'
 import NoMatch from './NoMatch'
 
 export default function App() {
@@ -15,12 +19,16 @@ export default function App() {
 
   return (
     <Routes>
-        <Route path='/' element={<NavBar />}>
-          <Route index element={<Home />} />
-          <Route path='testing' element={<h1>Test Route</h1>} />
+      <Route path='/' element={<NavBar />}>
+        <Route index element={<Home />} />
+        <Route path='testing' element={<h1>Test Route</h1>} />
+        <Route path='signup' element={<SignUp />} />
+        <Route path='signin' element={<SignIn />} />
+        <Route path='my-trips' element={<MyTrips />} />
+        <Route path='all-trips' element={<AllTrips />} />
 
-          <Route path='*' element={<NoMatch />} />
-        </Route>
+        <Route path='*' element={<NoMatch />} />
+      </Route>
     </Routes>
   )
 }
