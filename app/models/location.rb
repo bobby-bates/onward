6528bc4916696deaa07d8b9dcfed25e29dfcf63a
Bobby Bates <robertfbatesjr@gmail.com>
2022-09-25 20:15:47 -0400

    add associations to models

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,4 @@
 class Location < ApplicationRecord
+  has_many :trips
+  has_many :users, through: :trips
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+
+  has_many :trips
+  has_many :locations, through: :trips
 end
