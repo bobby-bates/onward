56a05c4f420d36711bd1b14475eb1b1cfa4a1811
Bobby Bates <robertfbatesjr@gmail.com>
2022-09-25 20:12:28 -0400

    add favorite migration, controller, model, and serializer

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :favorites
   resources :trips
   resources :locations
   resources :users
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/migrate/20220925221756_create_trips.rb
+++ db/migrate/20220925221756_create_trips.rb
@@ -4,6 +4,8 @@ class CreateTrips < ActiveRecord::Migration[7.0]
       t.string :description
       t.date :start_on
       t.date :end_on
+      t.belongs_to :user
+      t.belongs_to :location
 
       t.timestamps
     end
